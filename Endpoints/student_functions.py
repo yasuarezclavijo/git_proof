3a640af95cc8f59393a6e17462e2961ee82369cc
Yeison Suarez <y.suarezc@uniandes.edu.co>
2025-08-15 20:03:32 -0500

    AB#1 Reestructuracion y ajustes de carpeta y blueprints

---
+++ Endpoints/student_functions.py
@@ -0,0 +1,28 @@
+import json
+import logging
+import azure.functions as func
+
+logger = logging.getLogger(__name__)
+
+bp = func.Blueprint()
+
+@bp.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )

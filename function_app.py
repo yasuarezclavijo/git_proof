c6afac8868645a4be010e5eeac73be8c990315dc
Yeison Suarez <y.suarezc@uniandes.edu.co>
2025-08-15 20:22:04 -0500

    AB#2 Endpoint de paises

--- function_app.py
+++ function_app.py
@@ -1,7 +1,9 @@
 import azure.functions as func
 import logging
 from Endpoints.student_functions import bp as student_functions_blueprint
-
+from Endpoints.countries_functions import bp as countries_function
+    
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 app.register_functions(student_functions_blueprint)
+app.register_functions(countries_function)
